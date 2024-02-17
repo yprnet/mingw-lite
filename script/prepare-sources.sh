5a9be2e79497dd31a447a6ae0ad41802d6ab9902
yprnet <yuanpeirong@qq.com>
2024-02-17 17:24:25 +0800

    build gcc11.2.0-nls-useYuanPeirongURL

--- script/prepare-sources.sh
+++ script/prepare-sources.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 cd "$_ASSETS_DIR"
 [[ -f "$_BINUTILS_ARCHIVE" ]] || curl -LO "http://ftp.gnu.org/gnu/binutils/$_BINUTILS_ARCHIVE"
 [[ -f "$_MINGW_ARCHIVE" ]] || curl -LO "https://downloads.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/$_MINGW_ARCHIVE"
-[[ -f "$_GCC_ARCHIVE" ]] || curl -LO "http://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/$_GCC_ARCHIVE"
+[[ -f "$_GCC_ARCHIVE" ]] || curl -LO "http://www.yuanpeirong.com/gcc/gcc-$GCC_VER/$_GCC_ARCHIVE"
 [[ -f "$_GMP_ARCHIVE" ]] || curl -LO "https://ftp.gnu.org/gnu/gmp/$_GMP_ARCHIVE"
 [[ -f "$_MPFR_ARCHIVE" ]] || curl -LO "https://ftp.gnu.org/gnu/mpfr/$_MPFR_ARCHIVE"
 [[ -f "$_MPC_ARCHIVE" ]] || curl -LO "https://ftp.gnu.org/gnu/mpc/$_MPC_ARCHIVE"
@@ -25,7 +25,7 @@ cd "$_BUILD_DIR"
 [[ -d "$_MAKE_DIR" ]] || bsdtar -xf "$_ASSETS_DIR/$_MAKE_ARCHIVE" --no-same-owner
 
 cd "$_GCC_DIR"
-patch -p1 <"$_PATCH_DIR/gcc11.2.0-fix-localedir.patch"
+#patch -p1 <"$_PATCH_DIR/gcc11.2.0-fix-localedir.patch"
 case "$_WIN32_WINNT" in
   0x0501|0x0502)
     # skeeto/w64devkit#58: disable UTF-8 manifest for XP
